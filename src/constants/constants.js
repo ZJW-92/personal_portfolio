3cd2db09793238d07a4fc2d11844172dee266297
Zhijie Wei <57632939+ZJW-92@users.noreply.github.com>
2023-01-18 14:30:14 +0100

    update constant.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -21,55 +21,15 @@ export const projects = [
   },
 
   {
-    title: 'Portfolio website ',
-    description: " My portfolio of my resume of introducing myself, my background, projects and technologies I have learned and used in the past 2022.",
-    image: '/images/5.png',
-    tags: ['React.js', 'Next.js', 'React-icons', 'styled components'],
-    visit: 'https://github.com/ZJW-92/personal_portfolio',
-    demo: 'https://zhijiewei.netlify.app/',
-    id: 2,
-  },
-
-  {
-    title: 'ModernApp application ',
-    description: "ModernApp is a full-fledged, cool-designed landing page of showcasing one software company's webpage.",
-    image: '/images/7.png',
-    tags: ['Figma', 'React .js', 'React-bootstrap', 'styled-components',],
-    visit: 'https://github.com/ZJW-92/modernApp',
-    demo: 'https://modern-mobile-app.netlify.app/',
-    id: 3,
-  },
-
-  {
-    title: 'Keep Fit fitness application ',
-    description: " A fitness training application that help you browse and workout 1000+ exercises and more.",
-    image: '/images/6.png',
-    tags: ['React.js', 'React-icons', 'material-UI', 'Rapid API'],
-    visit: 'https://github.com/ZJW-92/keep_fit_fitness_app',
-    demo: 'https://keepfit-fitness.netlify.app/',
-    id: 4,
-  },
-
-  {
-    title: 'Shanghai kitchen UI/UX application',
-    description: "A modern and fully responsive UI/UX restaurant menu application with react-icons and CSS. ",
-    image: '/images/0.png',
-    tags: ['Figma','React', 'React-icons', 'CSS'],
-    visit: 'https://github.com/ZJW-92/Gourment_Shanghai_kitchen',
-    demo: 'https://shanghai-kitchen.netlify.app/',
-    id: 5,
-  },
-
-  {
     title: 'Full-stack MERN project',
     description: "A fullstack MERN application combined with Mongodb, ExpressJS, React and Node.js application to post a memory, edit, delete and count thumb up button.",
     image: '/images/1.png',
     tags: ['Mongodb', 'Express', 'React', 'Node.js'],
     visit: 'https://github.com/ZJW-92/FullStack_project',
     demo: 'https://memories-fullstackapp.netlify.app/',
-    id: 6,
+    id: 2,
   },
-  
+
   {
     title: 'Robot Framework Automation exercise',
     description:"Basic web automation testing using Selenium Library and Python. It includes test cases, mouse actions, buttons and connects with Jenkins to implement basic pipeline script. ",
@@ -77,18 +37,9 @@ export const projects = [
     tags: ['Robot Framework', 'Pycharm', 'Jenkins', 'Selenium'],
     visit: 'https://github.com/ZJW-92/RobotFramework_exercise',
     demo: 'https://github.com/ZJW-92/RobotFramework_exercise',
-    id: 7,
+    id: 3,
   },
-  
-  {
-    title: 'Big data COVID-19 analysis',
-    description:"Big data analysis of COVID-19 epidemic worldwide through daily data which ranged from January 2020 to March 2021 from Johns Hopkins University. It visualizes how much and fast this epidemic spreads around the world. ",
-    image: '/images/10.png',
-    tags: ['Jupyter notebook', 'pandas', 'pyecharts'],
-    visit: 'https://github.com/ZJW-92/bigData_COVID_Analysis',
-    demo: 'https://github.com/ZJW-92/bigData_COVID_Analysis',
-    id: 8,
-  },
+
 
   {
     title: 'Virtual Self-driving Vehicle ',
@@ -97,7 +48,7 @@ export const projects = [
     tags: ['C++', 'Git','Linux', 'Docker','OpenCV'],
     visit: 'https://github.com/ZJW-92/DIT638-Cyber-Physical-Systems-and-Systems-of-Systems',
     demo: 'https://github.com/ZJW-92/DIT638-Cyber-Physical-Systems-and-Systems-of-Systems',
-    id: 9,
+    id: 4,
   },
   
   {
@@ -107,6 +58,59 @@ export const projects = [
     tags: ['C++', 'Java', 'Travis CI','Arduino','SmartCar shield'],
     visit: 'https://github.com/ZJW-92/DIT112-V20-Miniprojekt-Systemutveckling/blob/main/README.md',
     demo: 'https://www.youtube.com/watch?v=pvArIKRuUSM',
+    id: 5,
+  },
+
+  {
+    title: 'Keep Fit fitness application ',
+    description: " A fitness training application that help you browse and workout 1000+ exercises and more.",
+    image: '/images/6.png',
+    tags: ['React.js', 'React-icons', 'material-UI', 'Rapid API'],
+    visit: 'https://github.com/ZJW-92/keep_fit_fitness_app',
+    demo: 'https://keepfit-fitness.netlify.app/',
+    id: 6,
+  },
+
+
+  {
+    title: 'ModernApp application ',
+    description: "ModernApp is a full-fledged, cool-designed landing page of showcasing one software company's webpage.",
+    image: '/images/7.png',
+    tags: ['Figma', 'React .js', 'React-bootstrap', 'styled-components',],
+    visit: 'https://github.com/ZJW-92/modernApp',
+    demo: 'https://modern-mobile-app.netlify.app/',
+    id: 7,
+  },
+
+
+  {
+    title: 'Shanghai kitchen UI/UX application',
+    description: "A modern and fully responsive UI/UX restaurant menu application with react-icons and CSS. ",
+    image: '/images/0.png',
+    tags: ['Figma','React', 'React-icons', 'CSS'],
+    visit: 'https://github.com/ZJW-92/Gourment_Shanghai_kitchen',
+    demo: 'https://shanghai-kitchen.netlify.app/',
+    id: 8,
+  },
+
+  
+  {
+    title: 'Big data COVID-19 analysis',
+    description:"Big data analysis of COVID-19 epidemic worldwide through daily data which ranged from January 2020 to March 2021 from Johns Hopkins University. It visualizes how much and fast this epidemic spreads around the world. ",
+    image: '/images/10.png',
+    tags: ['Jupyter notebook', 'pandas', 'pyecharts'],
+    visit: 'https://github.com/ZJW-92/bigData_COVID_Analysis',
+    demo: 'https://github.com/ZJW-92/bigData_COVID_Analysis',
+    id: 9,
+  },
+
+  {
+    title: 'Portfolio website ',
+    description: " My portfolio of my resume of introducing myself, my background, projects and technologies I have learned and used in the past 2022.",
+    image: '/images/5.png',
+    tags: ['React.js', 'Next.js', 'React-icons', 'styled components'],
+    visit: 'https://github.com/ZJW-92/personal_portfolio',
+    demo: 'https://zhijiewei.netlify.app/',
     id: 10,
   },
   
