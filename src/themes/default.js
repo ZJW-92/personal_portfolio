0c4eb1f5f6a8a7f9c894b369b0b5b0350e9a852c
ZJW-92 <zjwei0201@gmail.com>
2023-02-02 13:50:32 +0100

    update theme default

--- src/themes/default.js
+++ src/themes/default.js
@@ -2,7 +2,7 @@ export default {
   // Temp fonts
   fonts: {
     title: "Space Grotesk, sans-serif",
-    main: "Space Grotesk, sans-serif" 
+    main: "Maven Pro, sans-serif" 
   },
   // Colors for layout
   colors: {
