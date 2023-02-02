e5b904b0eb32eefc8b090d0166074c7e97269c28
ZJW-92 <zjwei0201@gmail.com>
2023-02-02 13:47:57 +0100

    update hero.js

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
-import { LeftSection } from './HeroStyles';
+import { LeftSection, Span } from './HeroStyles';
 
 const Hero = (props) => (  
   <Section row nopadding>
    <LeftSection> 
    <SectionTitle main center>
-     Welcome To Zhijie's <br />
-     Portfolio 
+     <Span>Welcome To Zhijie's Portfolio </Span>
      <img src="https://media2.giphy.com/media/ZGHpWzdOEkMKtwLqdc/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif" width="60" />
     </SectionTitle>
     <SectionText>  
