5ddee1b5d98071ed1378ea36ad793d9868602568
ZJW-92 <zjwei0201@gmail.com>
2023-02-02 13:50:04 +0100

    update document font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -31,7 +31,7 @@ export default class MyDocument extends Document {
     return (
       <Html lang='en-GB'>
         <Head>
-          <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
+          <link href="https://fonts.googleapis.com/css2?family=Liu+Jian+Mao+Cao&family=Maven+Pro&family=Pacifico&family=Space+Grotesk:wght@300;400;500;600;700&display=swap"rel="stylesheet"/>
         </Head>
         <body>
           <Main />
